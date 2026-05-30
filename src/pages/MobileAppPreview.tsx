39d1a80026e9c2af2353fa2ccb87c8dcb5110856
trippy2283 <gallowaytrippy22@gmail.com>
2026-05-29 23:22:05 -0500

    Add placeholder MobileAppPreview.tsx page

---
+++ src/pages/MobileAppPreview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MobileAppPreview = () => {
+  return (
+    <div>
+      <h1>Mobile App Preview</h1>
+      <p>This is the placeholder for the Mobile App Preview page.</p>
+    </div>
+  );
+};
+
+export default MobileAppPreview;
